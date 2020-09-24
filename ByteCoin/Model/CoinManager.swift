19776f37c75dafe3d0b08ae6b1e0e7ff39bbbfca
Tyler Christian <tylerc71197@gmail.com>
2020-09-24 13:47:01 -0400

    we are now printing data from the api into the console when we select a currency amount from the picker

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,11 +11,25 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "my api key here"
+    
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)\(currency)?apikey=\(apiKey)"
         
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print("error in coinPrice func \(error!)")
+                    return
+                }
+                let goodData = String(data: data!, encoding: .utf8)!
+                print(goodData)
+            }
+            task.resume()
+        }
     }
 }
